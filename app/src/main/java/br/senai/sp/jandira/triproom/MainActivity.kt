1b880eaba6a0730007e3ebda779fc1b01924c34b
OSWALDO DA SILVA BARBOSA DOS SANTOS <22282212@dsn127m1246727.cfp127aluno.local>
2023-03-07 16:44:03 -0300

    aula02

---
+++ app/src/main/java/br/senai/sp/jandira/triproom/MainActivity.kt
@@ -0,0 +1,116 @@
+package br.senai.sp.jandira.triproom
+
+import android.graphics.drawable.Icon
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.senai.sp.jandira.triproom.ui.theme.TripRoomTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TripRoomTheme {
+                TripRoomScreen()
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun TripRoomScreen() {
+
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+
+            //Header
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    Surface(
+                        modifier = Modifier
+                            .width(130.dp)
+                            .height(40.dp),
+                        color = Color(207, 6, 240),
+                        shape = RoundedCornerShape(bottomStart = 12.dp)
+                    ) {}
+                }
+
+                Spacer(modifier = Modifier.height(120.dp))
+
+                //Form
+                Column(
+                    modifier = Modifier
+                        .padding(start = 16.dp)
+                        .fillMaxWidth()
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.login),
+                        color = Color(207, 6, 240),
+                        fontSize = 48.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = stringResource(id = R.string.please),
+                        color = Color(160, 156, 156),
+                        fontSize = 14.sp
+                    )
+
+                    Spacer(modifier = Modifier.height(100.dp))
+
+                    OutlinedTextField(
+                        value = " ",
+                        label = { Text(text = "E-mail") },
+                        onValueChange = {},
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(end = 16.dp),
+                        shape = RoundedCornerShape(16.dp),
+                        leadingIcon = Icon.
+                    )
+
+                    Spacer(modifier = Modifier.height(32.dp))
+
+                    OutlinedTextField(
+                        value = " ",
+                        label = { Text(text = "E-mail") },
+                        onValueChange = {},
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(end = 16.dp),
+                        shape = RoundedCornerShape(16.dp)
+                    )
+
+                }
+            }
+        }
+    }
+}
